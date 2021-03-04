9905d756100144b861015303e9027f1cabb66746
Paweł Gargula <pawel.gargula@interia.pl>
2021-03-04 19:23:22 +0100

    Create class Timer and add some functions in js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 class Timer {
-    constructor(initialValue, startStopButton, resetButton) {
+    constructor(initialValue, clock, startStopButton, resetButton) {
         this.initialValue = initialValue;
+        this.clock = clock;
         this.startStopButton = startStopButton;
         this.resetButton = resetButton;
         this.presentValue = initialValue;
@@ -17,35 +18,53 @@ class Timer {
     }
 
     get startButtonVisible() {
-        this.startStopButton.classList.contains("start")
+        return this.startStopButton.classList.contains("start");
     }
 
     start() {
-        this.startButtonVisible = false;
         replaceClass(this.startStopButton, 'start', 'stop');
-
+        this.startStopButton.innerHTML = "Stop";
     }
 
     stop() {
-        this.startButtonVisible = true;
         replaceClass(this.startStopButton, 'stop', 'start');
+        this.startStopButton.innerHTML = "Start";
     }
 
     reset() {
-        this.presentValue = this.initialValue; 
+        replaceClass(this.startStopButton, 'stop', 'start');
+        this.startStopButton.innerHTML = "Start";
+        this.clock.innerHTML = convertMiliseconds(this.initialValue);
     }
 }
 
 //25 minutes in miliseconds
 const timerInitialValue = 25*60*1000;
 
+const clock = document.getElementById('clock');
 const startStopButton = document.getElementById('start-stop');
 const resetButton = document.getElementById('reset');
 
-const timer = new Timer(timerInitialValue, startStopButton, resetButton);
+const timer = new Timer(timerInitialValue, clock, startStopButton, resetButton);
 
 startStopButton.addEventListener('click', () => timer.startStop());
 resetButton.addEventListener('click', () => timer.reset());
+
+console.log(convertMiliseconds(timerInitialValue));
+
+function convertMiliseconds(miliseconds) {
+    let minutes = Math.floor((miliseconds % (1000 * 60 * 60)) / (1000 * 60));
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    let seconds = Math.floor((miliseconds % (1000 * 60)) / 1000);
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    return minutes + ":" + seconds;
+}
 
 function replaceClass(element, fromClass, toClass) {
     element.classList.remove(fromClass);
