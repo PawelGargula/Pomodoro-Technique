6cd8bc836aec74bb2bb0cd216b67ac404ad78c96
Paweł Gargula <pawel.gargula@interia.pl>
2021-03-17 18:13:31 +0100

    Add In class CountdownTimer method set(minutes)

--- main.js
+++ main.js
@@ -1,67 +1,111 @@
-class StartStopButton {
-    constructor() {
-        this.element = document.getElementById('start-stop');
-    }
-
-    update(to) {
-        this.element.setAttribute("class", to);
-        this.element.innerText = to.charAt(0).toUpperCase() + to.slice(1);
+class Handle {
+    constructor(id) {
+        this.handle = document.getElementById(id);
     }
 }
 
-class CountdownTimer {
-    constructor(startStopButton) {
-        this.startStopButton = startStopButton;
-        this.minutesElement = document.getElementById('minutes');
-        this.secondsElement = document.getElementById('seconds');
-        this.minutesInitialValue = "25";
-        this.secondsInitialValue = "00";
-        this.timerUpdater;
-        this.counter = 1;
-        this.alarm = new Audio('sound/alarm.flac'); //sound from https://freesound.org/s/22627/
+class Clock extends Handle {
+    constructor(id) {
+        super(id);
+    }
+}
+
+class Seconds extends Handle {
+    constructor(id) {
+        super(id);
+    }
+    
+    update(value) {
+        this.handle.innerText = (value < 10) ? `0${value}` : value;
+    }
+    
+    countToMiliseconds() {
+        return parseInt(this.handle.innerText, 10) * 1000;
+    }
+    
+    countToClockSeconds(miliseconds) {
+        return Math.floor((miliseconds % (1000*60)) / 1000);
+    }
+}
+
+class Minutes extends Seconds {
+    constructor(id) {
+        super(id);
+    }
+    
+    countToMiliseconds() {
+        return super.countToMiliseconds() * 60;
+    }
+    
+    countToClockMinutes(miliseconds) {
+        return Math.floor(miliseconds / (1000 * 60));
+    }
+}
+
+class StartStopButton extends Handle {
+    constructor(id) {
+        super(id);
+    }
+    
+    updateToStop() {
+        this.handle.className = 'stop';
+        this.handle.innerText = 'Stop';
     }
 
-    startStop() {
-        if (this.startStopButton.element.classList.contains("start")) {
-            this.start();
+    updateToStart() {
+        this.handle.className = 'start';
+        this.handle.innerText = 'Start';
+    }
+}
+
+class ResetButton extends Handle {
+    constructor(id) {
+        super(id);
+    }
+}
+
+class Counter extends Handle {
+    constructor(id) {
+        super(id);
+    }
+
+    update() {
+        if (this.value >= 4) {
+            this.handle.innerText = 1;
+        } else {
+            this.handle.innerText = parseInt(this.handle.innerText, 10) + 1;
         }
-        else {
-            this.stop();
+    }
+
+    get value() {
+        return parseInt(this.handle.innerText, 10);
+    }
+}
+
+class Message extends Handle {
+    constructor(id) {
+        super(id);
+    }
+
+    update(counter) {
+        if (counter >= 4) {
+            this.handle.innerText = 'Time for long break (15-30min)';
+        } else {
+            this.handle.innerText = 'Time for short break (3-5min)';
         }
     }
 
-    start() {
-        document.getElementById('message').innerText = "Message";
-        this.startStopButton.update('stop');
-        //Give a few miliseconds for program so then it can update clockElement correct after first second from start
-        let timeEnd = this.presentTime + countMiliseconds(parseInt(this.minutesElement.innerText), parseInt(this.secondsElement.innerText)) + 30; 
-        this.timerUpdater = setInterval(() => this.updateTimer(timeEnd), 1000);
+    reset() {
+        this.handle.innerText = "Message";
+    }
+}
+
+class Alarm {
+    constructor(src) {
+        this.alarm = new Audio(src);
     }
 
-    get presentTime() {
-        return new Date().getTime();
-    }
-
-    updateTimer(timeEnd) {
-        let distanceToTimeEnd = timeEnd - this.presentTime;
-        this.updateClock(distanceToTimeEnd);
-        document.title = `${document.getElementById('clock').innerText} - Pomodoro Timer`;
-
-        if (distanceToTimeEnd < 0) {
-            this.counter++;
-            this.playAlarm();
-            this.showMessage();
-            this.reset();
-            document.getElementById('counter').innerText = `Counter: ${this.counter}`;
-        }
-    }
-
-    updateClock(distanceToTimeEnd) {
-        this.minutesElement.innerText = convertToMinutesForClock(distanceToTimeEnd);
-        this.secondsElement.innerText = convertToSecondsForClock(distanceToTimeEnd);
-    }
-
-    playAlarm() {
+    play3Times() {
         this.alarm.play();
         let count = 1;
         this.alarm.onended = function() {
@@ -71,48 +115,99 @@ class CountdownTimer {
             }
         }
     }
+}
 
-    showMessage() {
-        if (this.counter > 4) {
-            document.getElementById('message').innerText = "Time for long break (15-30min)";
-            this.counter = 1;
+class CountdownTimer {
+    constructor(startStopButton) {
+        this.startStopButton = startStopButton;
+        this.seconds = new Seconds('seconds');
+        this.minutes = new Minutes('minutes');
+        this.counter = new Counter('counter');
+        this.message = new Message('message');
+        this.alarm = new Alarm('sound/alarm.flac'); //sound from https://freesound.org/s/22627/
+        this.minutesFromUser = 25;
+        this.timerInterval;
+    }
+
+    set(minutesFromUser) {
+        this.minutesFromUser = minutesFromUser;
+        this.reset();
+    }
+
+    startOrStop() {
+        if (startStopButton.handle.classList.contains('start')) {
+            this.startStopButton.updateToStop();
+            this.message.reset();
+            this.start();
+        } else {
+            this.startStopButton.updateToStart();
+            this.stop();
         }
-        else {
-            document.getElementById('message').innerText = "Time for short break (3-5min)";
+    }
+
+    start() {
+        //Give a few miliseconds for program so then it can update clockElement correct after first second from start
+        let timeEnd = this.now + this.minutes.countToMiliseconds() + this.seconds.countToMiliseconds() + 30;
+        this.timerInterval = setInterval(() => this.update(timeEnd), 1000);
+    }
+
+    get now() {
+        return new Date().getTime();
+    }
+
+    update(timeEnd) {
+        let distanceToTimeEnd = timeEnd - this.now;
+        this.updateClock(distanceToTimeEnd);
+        updatePageTitle();
+
+        if (distanceToTimeEnd < 0) {
+            this.alarm.play3Times();
+            this.message.update(this.counter.value);
+            this.counter.update();
+            this.reset();
         }
+    }
+
+    updateClock(distanceToTimeEnd) {
+        let seconds = this.seconds.countToClockSeconds(distanceToTimeEnd);
+        this.seconds.update(seconds);
+        let minutes = this.minutes.countToClockMinutes(distanceToTimeEnd);
+        this.minutes.update(minutes);
     }
 
     reset() {
+        this.minutes.update(this.minutesFromUser);
+        this.seconds.update(0);
         this.stop();
-        this.minutesElement.innerText = this.minutesInitialValue;
-        this.secondsElement.innerText = this.secondsInitialValue;
     }
 
     stop() {
-        clearInterval(this.timerUpdater);
-        this.startStopButton.update('start');
-        document.title = "Pomodoro Technique";
+        clearInterval(this.timerInterval);
+        this.startStopButton.updateToStart();
+        updatePageTitle();
     }
+
 }
 
-const startStopButton = new StartStopButton();
+const startStopButton = new StartStopButton('start-stop');
 const countdownTimer = new CountdownTimer(startStopButton);
-const resetButton = document.getElementById('reset');
-startStopButton.element.addEventListener('click', () => countdownTimer.startStop());
-resetButton.addEventListener('click', () => countdownTimer.reset());
+const resetButton = new ResetButton('reset');
+const clock = new Clock('clock');
 
+startStopButton.handle.addEventListener('click', () => countdownTimer.startOrStop());
 
-function countMiliseconds(minutes, seconds) {
-    let miliseconds = (minutes*60 + seconds)*1000;
-    return miliseconds;
-}
+resetButton.handle.addEventListener('click', () => countdownTimer.reset());
 
-function convertToMinutesForClock(miliseconds) {
-    let minutes = Math.floor((miliseconds % (1000*60*60)) / (1000*60));
-    return (minutes < 10) ? `0${minutes}` : minutes;
-}
+clock.handle.addEventListener('click', () => {
+    let minutes = prompt("Enter minutes (1-99)", "25");
+    minutes = parseInt(minutes, 10);
+    if (isNaN(minutes) || minutes < 1 || minutes > 99) {
+        console.log("Niepoprawne dane")
+    } else {
+        countdownTimer.set(minutes);
+    }
+});
 
-function convertToSecondsForClock(miliseconds) {
-    let seconds = Math.floor((miliseconds % (1000*60)) / 1000);
-    return (seconds < 10) ? `0${seconds}` : seconds;
+function updatePageTitle() {
+    document.title = `${clock.handle.innerText} - Pomodoro Timer`;
 }
