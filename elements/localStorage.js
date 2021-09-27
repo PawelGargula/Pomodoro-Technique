f28dca4fbd68f6e56ea3e370db069dd8e7895181
Paweł Gargula <pawel.gargula@interia.pl>
2021-09-27 20:05:00 +0200

    Plan how to use localStorage

---
+++ elements/localStorage.js
@@ -0,0 +1,28 @@
+export let storage = {
+    minutesKey: "minutes",
+
+    getMinutes() {
+        if (!isStorageSupported())
+            return 25;
+
+        let minutes = localStorage.getItem(this.minutesKey);
+        return minutes === null ? 25 : Number(minutes);
+    },
+
+    setMinutes(value) {
+        if (!isStorageSupported())
+            return;
+
+        localStorage.setItem(this.minutesKey, value);
+    }
+}
+
+function isStorageSupported() {
+    try {
+        localStorage.setItem("test", "test");
+        localStorage.removeItem("test");
+        return true;
+    } catch (err) {
+        return false;
+    }
+}
