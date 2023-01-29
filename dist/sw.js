686fec5e0a9f217e88eeda3f671f05a25a8e8154
Paweł Gargula <pawel.gargula@interia.pl>
2023-01-29 13:32:15 +0100

    Add service worker, which will serve cached files first.

---
+++ dist/sw.js
@@ -0,0 +1,57 @@
+// Update cacheName when resources change
+// it will add newly resources
+// and remove old one in our Cache Storage
+// naming convension is: resources-yy-mm-dd_hh:mm
+// Push this file after when changed resources are already deployed in server
+const cacheName = 'resources-2023-01-29_13:29';
+
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(resources);
+};
+
+const pathName = location.pathname;
+const path = pathName === '/'
+    ? '/'
+    : `${pathName.split('/')[0]}/${pathName.split('/')[1]}/`;
+const origin = `${location.origin}${path}`;
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        addResourcesToCache([
+            // paths needs to be written relative to the origin, not app's root directory
+            `${origin}`,
+            `${origin}index.html`,
+            `${origin}index.74294d40.css`,
+            `${origin}index.da045f6a.js`,
+            `${origin}index.55b71553.js`,
+        ])
+    );
+});
+
+const cacheFirst = async (request) => {
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+        return responseFromCache;
+    }
+    return fetch(request);
+};
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(cacheFirst(event.request));
+});
+
+const deleteCache = async key => {
+    await caches.delete(key)
+}
+
+const deleteOldCaches = async () => {
+    const cacheKeepList = [cacheName];
+    const keyList = await caches.keys()
+    const cachesToDelete = keyList.filter(key => !cacheKeepList.includes(key))
+    await Promise.all(cachesToDelete.map(deleteCache));
+}
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(deleteOldCaches());
+});
--- src/main.js
+++ src/main.js
@@ -14,3 +14,23 @@ startStopButton.handle.addEventListener("click", () =>
 resetButton.handle.addEventListener("click", () => countdownTimer.reset());
 
 clock.handle.addEventListener("click", () => countdownTimer.set());
+
+const registerServiceWorker = async () => {
+    if ('serviceWorker' in navigator) {
+        try {
+            // path needs to be written relative to the origin, app's root directory
+            const registration = await navigator.serviceWorker.register(`${location.href}sw.js`);
+            if (registration.installing) {
+                console.log('Service worker installing');
+            } else if (registration.waiting) {
+                console.log('Service worker installed');
+            } else if (registration.active) {
+                console.log('Service worker active');
+            }
+        } catch (error) {
+            console.error(`Registration failed with ${error}`);
+        }
+    }
+};
+
+registerServiceWorker();
